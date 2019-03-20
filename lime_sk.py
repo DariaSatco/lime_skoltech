6212cbc213f3f0c51d4d337733329f43f4f2d247
DariaSatco <dasha.shatco@gmail.com>
2019-03-20 16:31:35 +0300

    added check for n_features number

--- lime_sk.py
+++ lime_sk.py
@@ -14,22 +14,22 @@ class Lime():
         if kernel is not None:
             self._kernel = kernel
         else:
-            self._kernel = lambda x: gaussian_kernel(x, kernel_width)        
+            self._kernel = lambda x: gaussian_kernel(x, kernel_width)
         if distance is None:
-            distance = 'euclidean' 
+            distance = 'euclidean'
         self._distance = distance
         self._random_state = random_state
-       
+
     def _weight_data(self, data, labels, weights):
         """weight data for futher lasso regularization
-        
+
         Arguments:
             data {np.array} -- samples
             labels {np.array} -- sample labels
             weights {np.array} -- sample weights
-        
+
         Returns:
-            Tuple(np.array, np.array) -- weighted data and weighted labels 
+            Tuple(np.array, np.array) -- weighted data and weighted labels
         """
 
         weighted_data = ((data - np.average(data, axis=0, weights=weights))
@@ -41,7 +41,7 @@ class Lime():
     def _feature_selection(self, data, labels, weights, n_features, method='lasso',
                            rf_n=200, rf_max_depth=5):
         """select most important features with lasso
-        
+
         Arguments:
             weighted_data {np.array} -- weighted data
             weighted_labels {np.array} -- weighted labels
@@ -49,7 +49,7 @@ class Lime():
             method {str} -- regularization method 'lasso' or 'random_forest'
             rf_n {int} -- number of estimators in random forest
             rf_max_depth -- max depth of estimators in random forest
-        
+
         Returns:
             np.array -- indexes of the most important features
         """
@@ -72,17 +72,17 @@ class Lime():
 
     def _perturb_data(self, features, n_samples,  model, placeholder=None ):
         """create smaples in the neighborhood of the explained instance
-        
+
         Arguments:
             features {np.array} -- simplified features of theoriginal instance
                                     usually is an array of ones
             n_samples {np.array} -- number of random samples to create
             model {callable} -- takes array of the implified features and returns
                                 probability of explained class
-        
+
         Keyword Arguments:
             placeholder {object} -- value to replace features
-        
+
         Returns:
             Tuple(np.array, np.array) -- perturbed data and labels in the neighborhood
                                          of the original data
@@ -105,10 +105,10 @@ class Lime():
 
         return result_data, labels
 
-    def explain(self, features, model, n, n_features, reg_method='lasso',
+    def explain(self, features, model, n, n_features=None, reg_method='lasso',
                 rf_n=200, rf_max_depth=5, regressor='ridge', dt_max_depth=10):
         """explain instance
-        
+
         Arguments:
             features {np.array} -- simplified features of theoriginal instance
                                     usually is an array of ones
@@ -130,22 +130,28 @@ class Lime():
         )
         distances = pairwise_distances(
             perturbed_data,
-            features.reshape(1, -1),     
+            features.reshape(1, -1),
             metric=self._distance
         ).ravel()
         weights = self._kernel(distances)
 
-        used_features = self._feature_selection(perturbed_data,
-        preturbed_labels, weights, n_features, reg_method, rf_n, rf_max_depth, )
+        if n_features == None:
+            used_features = list(range(features.size))
+        else:
+            used_features = self._feature_selection(perturbed_data,
+                                                    preturbed_labels,
+                                                    weights, n_features,
+                                                    reg_method, rf_n, 
+                                                    rf_max_depth, )
 
-        if regressor == 'ridge':        
+        if regressor == 'ridge':
             explainer_model = Ridge(alpha=0.1, random_state=self._random_state)
 
             explainer_model.fit(perturbed_data[:, used_features], preturbed_labels,
                                 sample_weight=weights)
 
             return (sorted(zip(used_features, explainer_model.coef_),
-                            key=lambda a: np.abs(a[1]), reverse=True), 
+                            key=lambda a: np.abs(a[1]), reverse=True),
                     explainer_model.score(perturbed_data[:, used_features], preturbed_labels,
                                             sample_weight=weights))
 
@@ -154,19 +160,9 @@ class Lime():
             explainer_model.fit(perturbed_data[:, used_features], preturbed_labels,
                                 sample_weight=weights)
             return (sorted(zip(used_features, explainer_model.feature_importances_),
-                           key=lambda a: np.abs(a[1]), reverse=True), 
+                           key=lambda a: np.abs(a[1]), reverse=True),
                     explainer_model.score(perturbed_data[:, used_features], preturbed_labels,
                                           sample_weight=weights))
 
 def gaussian_kernel(x, sigma):
     return np.exp(-(x ** 2) / (sigma ** 2))
-
-
-
-
-
-
-        
-        
-
-        
